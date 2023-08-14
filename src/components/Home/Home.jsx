3a13f9bfcec6bad93d16a9da1811ff59cd228bff
andrenaP <a0993100106@gmail.com>
2023-08-14 11:30:09 +0300

    test

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,31 +1,37 @@
 // import DataForomFIle from '../json/All';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import ApiServiceClass from '../service';
-const ApiService = new ApiServiceClass();
+// import ApiServiceClass from '../service';
+// const ApiService = new ApiServiceClass();
 
 const PosterFirstPart = 'https://image.tmdb.org/t/p/w220_and_h330_face/';
 
 export function Home() {
   const [movies, setMovies] = useState([]);
   // const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
   // console.log(DataForomFIle);
   // const books = DataForomFIle.results;
 
   useEffect(() => {
-    setLoading(true);
-    try {
-      ApiService.All()
-        .then(Books => {
-          setMovies(Books.results);
-        })
-        .finally(setLoading(false));
-    } catch (e) {
-      console.log(e);
-      console.log(loading);
-    }
+    // setLoading(true);
+
+    const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTA2OTg2YTMwZWVkNDNmMjRmZjJhZmE5ZGY0N2FiOCIsInN1YiI6IjY0ZDg4MzEzZDEwMGI2MDExYzgyM2U2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a8rp64-ZzW-scI2R6ybCgpdPObOATyZzVgg59Sy_an8',
+      },
+    };
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(json => setMovies(json.results))
+      .catch(err => console.error('error:' + err));
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
